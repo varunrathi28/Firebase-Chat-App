ec2c31282a631766c8a09a157f8d4a074dd5d379
Varun Rathi <varunrathi28@gmail.com>
2017-05-08 01:51:13 +0530

    Chat messge update bug fixed and chat cell added \n Snapkit pod removed

---
+++ FireChat/ChatMessageCell.swift
@@ -0,0 +1,13 @@
+//
+//  ChatMessageCell.swift
+//  FireChat
+//
+//  Created by Varun Rathi on 08/05/17.
+//  Copyright © 2017 vrat28. All rights reserved.
+//
+
+import UIKit
+
+class ChatMessageCell: UICollectionViewCell {
+    
+}
